ac11f5c23df3bda31c13406f67e2558857ece118
Javi <javsolgar@alum.us.es>
2019-11-03 23:16:21 +0100

    the system store spam words and a spam threshold

--- src/main/population/create-schema.sql
+++ src/main/population/create-schema.sql
@@ -57,6 +57,18 @@
         primary key (`id`)
     ) engine=InnoDB;
 
+    create table `configuration` (
+       `id` integer not null,
+        `version` integer not null,
+        `spam_threshold` double precision,
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `configuration_spam_words` (
+       `configuration_id` integer not null,
+        `spam_words` varchar(255)
+    ) engine=InnoDB;
+
     create table `consumer` (
        `id` integer not null,
         `version` integer not null,
@@ -154,6 +166,11 @@
        add constraint FK_h52w0f3wjoi68b63wv9vwon57 
        foreign key (`user_account_id`) 
        references `user_account` (`id`);
+
+    alter table `configuration_spam_words` 
+       add constraint `FK5lk29cpqe3960a943x8x8j4yh` 
+       foreign key (`configuration_id`) 
+       references `configuration` (`id`);
 
     alter table `consumer` 
        add constraint FK_6cyha9f1wpj0dpbxrrjddrqed 
